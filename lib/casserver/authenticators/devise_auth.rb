b280dcf9a3a5094b98a49c4e5b860961a7f56b76
Akash Manohar J <akash@akash.im>
2011-06-20 19:23:16 +0530

    fixed devise auth error for wrong credentials. doesn't throw errors anymore
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- lib/casserver/authenticators/devise_auth.rb
+++ lib/casserver/authenticators/devise_auth.rb
@@ -13,11 +13,22 @@ class CASServer::Authenticators::DeviseAuth < CASServer::Authenticators::Base
   def validate(credentials)
 
   	read_standard_credentials(credentials)
-	  $LOG.debug(@request['warden'].inspect)
+	$LOG.debug(@request['warden'].inspect)
 
-    # set the field name in the config.yml file    
-    return true if (@request['warden'].authenticated? or User.find_by_email(@username).valid_password?(@password))
-	  false
+    # TODO allow set the field name in the config.yml file
+    # That should allow to choose between email or username. LOW PRIORITY
+      
+    if @request['warden'].authenticated?
+        return true
+    elsif user=User.find_by_email(@username) and user.respond_to?(:valid_password?)
+      # Well you could just check the find count. But you'll be making the find call again in this block
+      # Oh an the single equals in the condition is for a purpose :)
+      
+      return true if user.valid_password?(@password)
+    end
+    
+    # default return false
+    false
   end
 
 end
