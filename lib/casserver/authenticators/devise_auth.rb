aca39ff730ecd6bd8695a6681c8f6a96c203a3d7
Akash Manohar J <akash@akash.im>
2011-08-02 20:07:55 +0530

    added username_column option support for Devise authenticator
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- lib/casserver/authenticators/devise_auth.rb
+++ lib/casserver/authenticators/devise_auth.rb
@@ -13,28 +13,32 @@ class CASServer::Authenticators::DeviseAuth < CASServer::Authenticators::Base
   def validate(credentials)
     read_standard_credentials(credentials)
   	
-    # TODO allow set the field name (email/username) in the config.yml file. LOW PRIORITY
+    # Assumes 'email' as the column name if not specified
+    username_column = @options[:username_column] || 'email'
+    results = User.find(:all, :conditions => ["#{username_column} = ?", @username,)
     
-    user = User.find_by_email(@username)
-    
-    # to check if find didn't return nil. can also do with !nil?
-    if user.try(:valid_password?, @password)
+    if results.length == 1
+      user = results.first
 
-      if @options[:extra_attributes].blank?
-        $LOG.warn "WTF, no extra attributes?"
-      else
-        $LOG.warn "FILLING IT UP"
-        @options[:extra_attributes].each do |method|
-          $LOG.warn "#{method} = #{user.send(method)}"
-          @extra_attributes[method] = user.send(method)
+      # to check if find didn't return nil. can also do with !nil?
+      if user.try(:valid_password?, @password)
+
+        if @options[:extra_attributes].blank?
+          $LOG.warn "WTF, no extra attributes?"
+        else
+          $LOG.warn "FILLING IT UP"
+          @options[:extra_attributes].each do |method|
+            $LOG.warn "#{method} = #{user.send(method)}"
+            @extra_attributes[method] = user.send(method)
+          end
         end
+
+        $LOG.warn "-"*30
+        $LOG.warn @extra_attribues.inspect
+        $LOG.warn "-"*30
+
+        return true
       end
-
-      $LOG.warn "-"*30
-      $LOG.warn @extra_attribues.inspect
-      $LOG.warn "-"*30
-
-      return true
     end
 
     false
